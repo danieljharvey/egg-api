2a1f513ca6caf53f0e6932af7ae2277a00539a76
Daniel Harvey <danieljamesharvey@gmail.com>
2020-01-18 13:05:15 +0000

    Rotate grid

--- lib/Egg/SampleProjections.hs
+++ lib/Egg/SampleProjections.hs
@@ -24,19 +24,32 @@ data Board
   deriving (Eq, Show, Generic, JSON.ToJSON, JSON.FromJSON)
 
 instance Arbitrary Board where
-  arbitrary = Board <$> arbitrary
+  arbitrary = do
+    items <- arbitrary
+    pure . coordListToBoard . boardToCoordList $ Board items
 
 data EggState
   = EggState
       {boards :: Map.Map BoardId Board}
   deriving (Eq, Show, Generic, JSON.ToJSON, JSON.FromJSON)
 
+newtype BoardSize
+  = BoardSize {getBoardSize :: Int}
+  deriving (Eq, Ord, Show)
+
 empty :: EggState
 empty = EggState mempty
 
-blankBoard :: Int -> Int -> Board
-blankBoard w h =
-  Board {tiles = replicate w (replicate h item)}
+data Coord
+  = Coord
+      { coordX :: Int,
+        coordY :: Int
+      }
+  deriving (Eq, Ord, Show, Generic)
+
+blankBoard :: BoardSize -> Board
+blankBoard (BoardSize w) =
+  Board {tiles = replicate w (replicate w item)}
   where
     item = TileId 0
 
@@ -47,15 +60,16 @@ mapWithIndex f as =
     as' =
       zip [0 ..] as
 
-setElem :: Int -> Int -> a -> [[a]] -> [[a]]
-setElem ax ay item old =
-  mapWithIndex
-    ( \x' row ->
-        if ax == x'
-          then changeRow row
-          else row
-    )
-    old
+setElem :: Coord -> TileId -> Board -> Board
+setElem (Coord ax ay) item (Board old) =
+  Board $
+    mapWithIndex
+      ( \x' row ->
+          if ax == x'
+            then changeRow row
+            else row
+      )
+      old
   where
     changeRow =
       mapWithIndex
@@ -64,6 +78,52 @@ setElem ax ay item old =
               then item
               else item'
         )
+
+boardToCoordList :: Board -> [(Coord, TileId)]
+boardToCoordList (Board as) = concat withCoords
+  where
+    withCoords =
+      mapWithIndex
+        ( \x' row ->
+            mapWithIndex (\y' item' -> ((Coord x' y'), item')) row
+        )
+        as
+
+sizeFromCoordList :: [(Coord, TileId)] -> BoardSize
+sizeFromCoordList as =
+  BoardSize
+    $ getMax
+    $ foldMap Max ((sizeCoord . fst) <$> as)
+  where
+    sizeCoord :: Coord -> Int
+    sizeCoord (Coord x' _) = x' + 1
+
+coordListToBoard :: [(Coord, TileId)] -> Board
+coordListToBoard as =
+  foldl
+    ( \board' (coord, tile) ->
+        setElem coord tile board'
+    )
+    emptyBoard
+    as
+  where
+    emptyBoard = blankBoard (sizeFromCoordList as)
+
+dontNeedToRotate :: Board -> Bool
+dontNeedToRotate (Board tiles') = length tiles' < 2
+
+rotateBoard :: RotateDirection -> Board -> Board
+rotateBoard direction board =
+  if dontNeedToRotate board
+    then board
+    else coordListToBoard rotatedCoordList
+  where
+    boardSize' =
+      sizeFromCoordList coordList
+    coordList =
+      boardToCoordList board
+    rotatedCoordList =
+      (\(coord, tile) -> (translateRotation boardSize' direction coord, tile)) <$> coordList
 
 expandBoard :: [[TileId]] -> [[TileId]]
 expandBoard as = (map (\a -> a <> [TileId 0]) as) <> [more]
@@ -76,20 +136,37 @@ shrinkBoard as =
   where
     size = max (length as - 1) 1
 
+translateRotation ::
+  BoardSize ->
+  RotateDirection ->
+  Coord ->
+  Coord
+translateRotation (BoardSize size') clockwise (Coord x' y') =
+  case clockwise of
+    Clockwise -> right
+    AntiClockwise -> left
+  where
+    boardSize' =
+      max (size' - 1) 1
+    right =
+      Coord (boardSize' - y') x'
+    left =
+      Coord y' (boardSize' - x')
+
 eggBoardProjection :: Projection BoardActions EggState
 eggBoardProjection =
   Projection
     { reducer = \action (EggState boards') -> case action of
-        NewBoardAction (NewBoard width' height' boardId') ->
+        NewBoardAction (NewBoard width' _ boardId') ->
           EggState
-            ( Map.singleton boardId' (blankBoard width' height')
+            ( Map.singleton boardId' (blankBoard (BoardSize width'))
                 <> boards'
             )
         AddTileAction (AddTile x' y' tileId' boardId') ->
           EggState
             ( Map.adjust
-                ( \(Board a) ->
-                    Board $ setElem x' y' tileId' a
+                ( \board' ->
+                    setElem (Coord x' y') tileId' board'
                 )
                 boardId'
                 boards'
@@ -109,6 +186,13 @@ eggBoardProjection =
                 ( \(Board a) ->
                     Board $ shrinkBoard a
                 )
+                boardId'
+                boards'
+            )
+        RotateBoardAction (RotateBoard boardId' direction) ->
+          EggState
+            ( Map.adjust
+                (\board' -> rotateBoard direction board')
                 boardId'
                 boards'
             ),
