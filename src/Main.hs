6c32917677a149db564f9a0d079aac82276024ce
Daniel Harvey <danieljamesharvey@gmail.com>
2020-03-25 09:18:28 +0000

    Use servant

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
 
 import Control.Monad.Reader
 import qualified Data.Aeson as JSON
@@ -13,13 +14,27 @@ import qualified Data.Text as Tx
 import qualified Database.PostgreSQL.Simple as SQL
 import qualified Egg.API as API
 import qualified Egg.EggM as Egg
+import qualified Egg.EventTypes as Actions
 import qualified Egg.SampleProjections as Sample
 import qualified MiniEventStore as MES
 import qualified Network.HTTP.Types as HTTP
 import qualified Network.Wai as Wai
 import qualified Network.Wai.Handler.Warp as Warp
 import Network.Wai.Middleware.Cors
+import Servant
+import Server.Types
 import qualified System.Envy as Envy
+
+type EggAPI = PostAPI Actions.BoardActions :<|> API.EggServerAPI Sample.EggState
+
+eggAPI :: Proxy EggAPI
+eggAPI = Proxy
+
+eggAPIServer :: Egg.EggConfig Actions.BoardActions Sample.EggState -> Server EggAPI
+eggAPIServer config = postAPI config :<|> API.eggServerAPI config
+
+eggApplication :: Egg.EggConfig Actions.BoardActions Sample.EggState -> Wai.Application
+eggApplication config = serve eggAPI (eggAPIServer config)
 
 main :: IO ()
 main = do
@@ -39,71 +54,7 @@ main = do
       Warp.runSettings settings (application eggConfig)
 
 application ::
-  (JSON.FromJSON action, Show state) =>
-  Egg.EggConfig action state ->
+  Egg.EggConfig Actions.BoardActions Sample.EggState ->
   Wai.Application
 application config =
-  simpleCors $
-    ( \request respond ->
-        runReaderT (Egg.runEggM (requestHandler request)) config
-          >>= respond
-    )
-
-requestHandler ::
-  ( JSON.FromJSON action,
-    Show state,
-    MonadIO m,
-    MES.GetEvents m,
-    MES.CacheState state m,
-    MES.WriteEvent m,
-    MonadReader (Egg.EggConfig action state) m
-  ) =>
-  Wai.Request ->
-  m Wai.Response
-requestHandler request =
-  if Wai.requestMethod request == HTTP.methodPost
-    then (liftIO $ Wai.requestBody request) >>= handlePostRequest
-    else handleGetRequest (Wai.pathInfo request)
-
--- post means plop an event in the store
-handlePostRequest ::
-  ( MES.WriteEvent m
-  ) =>
-  BS8.ByteString ->
-  m Wai.Response
-handlePostRequest jsonStr = do
-  -- write the event
-  MES.writeEvent jsonStr
-  let status = HTTP.status200
-  let headers = []
-  let body = "Saved!"
-  pure (Wai.responseLBS status headers body)
-
-handleGetRequest ::
-  ( JSON.FromJSON action,
-    MES.GetEvents m,
-    MES.CacheState state m,
-    MonadReader (Egg.EggConfig action state) m
-  ) =>
-  [Tx.Text] ->
-  m Wai.Response
-handleGetRequest args = do
-  -- lets assume this is get and try and access the API
-  projection' <- asks Egg.projection
-  api' <- asks Egg.api
-  response <- MES.runAPIRequest projection' api' args
-  case response of
-    Just a ->
-      pure
-        ( Wai.responseLBS
-            HTTP.status200
-            [(HTTP.hContentType, "application/json")]
-            (JSON.encode a)
-        )
-    Nothing ->
-      pure
-        ( Wai.responseLBS
-            HTTP.status400
-            []
-            "No response!"
-        )
+  simpleCors (eggApplication config)
